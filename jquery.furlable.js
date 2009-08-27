a986231901f6167bf4ffd18acda240d6daa3372a
Thomas Jachmann <tom.j@gmx.net>
2009-08-28 00:26:43 +0200

    working!!

---
+++ jquery.furlable.js
@@ -0,0 +1,99 @@
+(function($) {
+  
+  // configure the global furlable namespace
+  $.furlable = {
+    // the defaults
+    defaults: {
+      max: 20,
+      hasMore: '...',
+      wrapperBefore: null,
+      wrapperAfter: null,
+      hasMoreDuration: 100,
+      htmlDuration: 500,
+      moreText: null,
+      lessText: null
+    },
+    
+    // the truncateHtml function used to truncate html
+    truncateHtml: function(html, length) {
+      return truncateNode($('<div/>').html(html), length).html();
+    },
+  };
+  
+  // private function to truncate any node
+  function truncateNode(node, length) {
+    return (node.nodeType == 3) ? truncateTextNode(node, length) : truncateOtherNode(node, length);
+  }
+  
+  // private function to truncate plain text nodes
+  function truncateTextNode(node, length) {
+    var truncated = node.data.replace(/\s+/g, ' ').slice(0, length);
+    return $('<div/>').text(truncated).html();
+  }
+  
+  // private function to truncate non plain text nodes
+  function truncateOtherNode(node, length) {
+    node = $(node);
+    var truncated = node.clone().empty();
+    var remaining = length;
+    node.contents().each(function() {
+      if (remaining > 0) {
+        var child = truncateNode(this, remaining);
+        if (child)
+          truncated.append(child);
+        remaining = length - truncated.text().length;
+      }
+    });
+    return truncated;
+  }
+  
+  // the furlable function to initialize furlables
+  $.fn.furlable = function(options) {
+    var o = $.extend({}, $.furlable.defaults, options);
+    
+    this.each(function() {
+      var $this = $(this);
+      
+      // determine parameters
+      var lo = o.hasMore ? -$('<div/>').html(o.hasMore).text().length : 0;
+      var hi = $this.text().length - o.max
+      var cur = hi;
+      var unfurled = $this.html();
+      
+      // setup furling
+      var furl = function(to) {
+        if (to != cur) {
+          var furled = '';
+          furled += o.wrapperBefore;
+          if (to < 0) {
+            furled += $.furlable.truncateHtml(unfurled, o.max);
+            furled += $.furlable.truncateHtml(o.hasMore, -to);
+          } else {
+            furled += $.furlable.truncateHtml(unfurled, o.max + to);
+          }
+          furled += o.wrapperAfter;
+          $this.html(furled);
+        }
+        cur = length;
+      };
+      furl(lo);
+      var furled = true;
+      
+      // setup animation
+      var furlimate = function(from, to) {
+        $({furlableCount: from}).animate({furlableCount: to}, {
+          duration: o.htmlDuration,
+          step: function() {
+            furl(Math.round(this.furlableCount));
+          }
+        });
+      };
+      $this.click(function() {
+        furled ? furlimate(lo, hi) : furlimate(hi, lo);
+        furled = !furled;
+      });
+    })
+    
+    return this;
+  };
+})(jQuery);
--- scribtles.js
+++ scribtles.js
@@ -1,90 +1,13 @@
-(function($) {
-  $.fn.animateText = function(options, callback) {
-    var defaults = {
-      duration: 500
-    };
-    var options = $.extend(defaults, options)
-    
-    var $this = $(this);
-    if (options.html == undefined) {
-      var html = '';
-      var text = this.text();
-      var from = text.length;
-      var to = 0;
-    } else {
-      var html = options.html;
-      var text = $('<div>' + html + '</div>').text();
-      var from = 0;
-      var to = text.length;
-    }
-    jQuery({animateTextCount: from}).animate({animateTextCount: to}, {
-      duration: options.duration,
-      step: function() {
-        $this.text(text.substring(0, Math.round(this.animateTextCount)));
-      },
-      complete: function() {
-        $this.html(html);
-        if (callback)
-          callback.call();
-      }
-    });
-    return this;
-  }
-  $.fn.shorten = function(options) {
-    var defaults = {
-      slicePoint: 20,
-      hasMore: '...',
-      wrapperBefore: null,
-      wrapperAfter: null,
-      hasMoreDuration: 100,
-      htmlDuration: 500
-    };
-    var options = $.extend(defaults, options);
-    
-    this.each(function() {
-      var $this = $(this);
-    
-      var allText = $this.html();
-      var startText = allText.slice(0, options.slicePoint).replace(/\w+$/,'');
-      startTags = startText.match(/<\w[^>]*>/g);
-      if (startTags) {
-        startText = allText.slice(0, options.slicePoint + startTags.join('').length).replace(/\w+$/,'');
-      }
-      if (startText.lastIndexOf('<') > startText.lastIndexOf('>') ) {
-        startText = startText.slice(0, startText.lastIndexOf('<'));
-      }
-      var endText = allText.slice(startText.length);
-    
-      var html = '';
-      html += options.wrapperBefore;
-      html += startText;
-      html += '<span class="expanded">' + options.hasMore + '</span>';
-      html += options.wrapperAfter;
-      $this.html(html);
-    
-      $this.data('expanded', false);
-    
-      $this.click(function() {
-        var expanded = $(this).data('expanded');
-        var span = $this.find('span.expanded');
-        if (expanded) {
-          span.animateText({duration: options.htmlDuration}, function() { span.animateText({html: options.hasMore, duration: options.hasMoreDuration}); });
-        } else {
-          span.animateText({duration: options.hasMoreDuration}, function() { span.animateText({html: endText, duration: options.htmlDuration}) });
-        }
-        $(this).data('expanded', !expanded);
-      });
-    })
-  };
-})(jQuery);
-
 $(document).ready(function() {
-  $('.digression').shorten({
+  $('.digression').furlable({
     wrapperBefore: '&lt;digression&gt;',
     wrapperAfter: '&lt;/digression&gt;'
   });
-  $('.expansion').shorten({
+  $('.expansion').furlable({
     wrapperBefore: '&lt;expansion&gt;',
-    wrapperAfter: '&lt;/expansion&gt;'
+    wrapperAfter: '&lt;/expansion&gt;',
+    htmlDuration: 2000,
+    moreText: 'more...',
+    lessText: 'less...',
   });
 });
